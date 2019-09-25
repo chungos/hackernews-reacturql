aaac0eda788b7a56ed0958e8abf903b07ea3f341
Owen Chung <Owen.Chung@gmail.com>
2019-09-25 20:01:26 +1000

    basic app folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../styles/App.css'
 
-function App() {
+fJnction App() {
   return (
     <div className="App">
       <header className="App-header">
